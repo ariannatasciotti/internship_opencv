1c8435beb11707020d520d4ddeb66650446db30a
Arianna Tasciotti <aria.tasc7@gmail.com>
2020-10-12 15:06:00 +0200

    exercise_0

---
+++ src/shape_based.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import numpy as np
+from utilities.shapedetectors import ShapeDetector
+image = cv.imread("A4_Shapes.png")
+hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+lower = np.array([20, 150, 100])
+upper = np.array([20, 255, 255])
+mask = cv.inRange(hsv, lower, upper)  # filtering yellow
+mask_img = cv.bitwise_and(image, image, mask=mask)   # bitwise conjunction of the two arrays image and image
+mask_img = 255 - mask
+cv.imshow("Original", image)
+cv.waitKey(5000) 
+cv.imshow("Mask", mask_img)
+cv.waitKey(5000) 
+cv.destroyAllWindows()
+
+contours, hierarchy = cv.findContours(mask_img.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+sd = ShapeDetector()
+for c in contours:
+    '''
+    M = cv.moments(c)
+    cX = int((M["m10"] / M["m00"]))
+    cY = int((M["m01"] / M["m00"]))
+    shape = sd.detect(c)
+    cv.putText(mask_img, shape, (cX, cY), cv.FONT_HERSHEY_SIMPLEX,
+		0.5, (0, 255, 0), 2)
+    cv.imshow("Image", mask_img)
+    cv.waitKey(7000) 
+    '''
+    x,y,w,h = cv.boundingRect(c) 
+    if w>250 and h>100 and w<260 and h<500:
+        new_img = mask_img[y:y+h, x:x+w]
+        cv.imwrite('Output_shape' + '.png', new_img) 
+cv.imshow("Output", new_img)
+cv.waitKey(9000)
